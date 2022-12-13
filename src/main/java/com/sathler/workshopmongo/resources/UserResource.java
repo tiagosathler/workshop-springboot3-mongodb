67cabbc250267dcaded339fafbefc47ecab1e6c7
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-13 10:32:33 -0300

    :sparkles: feat: Usando padrão DTO para retornar usuários

--- src/main/java/com/sathler/workshopmongo/resources/UserResource.java
+++ src/main/java/com/sathler/workshopmongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sathler.workshopmongo.domain.User;
+import com.sathler.workshopmongo.dto.UserDTO;
 import com.sathler.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +20,11 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = service.findAll();
+		
+		List<UserDTO> usersDTO = users.stream().map(UserDTO::new).toList();
 
-		return ResponseEntity.ok().body(users);
+		return ResponseEntity.ok().body(usersDTO);
 	}
 }
