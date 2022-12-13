a26206104069bed2715fee9cae70a9f9e75bf745
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-13 20:03:14 -0300

    :sparkles: feat: Projeção dos dados do autor com DTO

--- src/main/java/com/sathler/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sathler/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sathler.workshopmongo.domain.Post;
 import com.sathler.workshopmongo.domain.User;
+import com.sathler.workshopmongo.dto.AuthorDTO;
 import com.sathler.workshopmongo.repositories.PostRepository;
 import com.sathler.workshopmongo.repositories.UserRepository;
 
@@ -28,26 +29,28 @@ public class Instantiation implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(
 				null,
 				sdf.parse("21/03/2018"),
 				"Partiu viagem",
 				"Vou viajar para São Paulo. Abraços!",
-				maria);
+				new AuthorDTO(maria));
 
 		Post post2 = new Post(
 				null,
 				sdf.parse("23/03/2018"),
 				"Bom dia",
 				"Arcordei feliz hoje!",
-				maria);
+				new AuthorDTO(maria));
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
