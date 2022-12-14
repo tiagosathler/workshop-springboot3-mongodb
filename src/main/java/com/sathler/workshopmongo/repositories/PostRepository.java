86b427d74fa6bea206bed776037bd8fef042bec3
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-14 11:56:34 -0300

    :sparkles: feat: Consulta com vários critérios

--- src/main/java/com/sathler/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/sathler/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.sathler.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,9 +9,22 @@ import org.springframework.data.mongodb.repository.Query;
 import com.sathler.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	static final String FULL_SEARCH_QUERY = """
+			{ $and: [
+				{ 'date': { $gte: ?1 } },
+				{ 'date': { $lte: ?2 } },
+				{ $or: [
+					{ 'title': { $regex: ?0, $options: 'i' }},
+					{ 'body': { $regex: ?0, $options: 'i' }},
+					{ 'comments.text': { $regex: ?0, $options: 'i' }},
+				] } ],
+			}""";
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 	@Query("{'title': { $regex: ?0, $options: 'i' }}")
 	List<Post> searchTitle(String text);
+
+	@Query(FULL_SEARCH_QUERY)
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/sathler/workshopmongo/resources/PostResource.java
+++ src/main/java/com/sathler/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.sathler.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,20 @@ public class PostResource {
 		text = URL.decodeParam(text);
 
 		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDateStr,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDateStr) {
+
+		text = URL.decodeParam(text);
+		Date minDate = URL.convertDate(minDateStr, new Date(0L));
+		Date maxDate = URL.convertDate(maxDateStr, new Date());
+
+		List<Post> posts = service.fullSearch(text, minDate, maxDate);
 		return ResponseEntity.ok().body(posts);
 	}
 }
