be317926f423029864a5d454b11d0dbbca16a3e0
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-14 08:41:36 -0300

    :sparkles: feat: Obtendo um post por id

---
+++ src/main/java/com/sathler/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.sathler.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sathler.workshopmongo.domain.Post;
+import com.sathler.workshopmongo.repositories.PostRepository;
+import com.sathler.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+
+		if (post.isEmpty()) {
+			throw new ObjectNotFoundException(("Post id '" + id + "' not found!"));
+		}
+
+		return post.get();
+	}
+}
